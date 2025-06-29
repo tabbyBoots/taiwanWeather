452cd7027796b209cb865c64e76e39b1e9203602
Boots <githubBoots@bloomski.com>
2025-06-30 00:18:23 +0800

    修正build沒有產生assets的問題

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/taiwanWeather/', // Set base path for GitHub Pages
   plugins: [vue()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets', 
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',
     port:5173,
